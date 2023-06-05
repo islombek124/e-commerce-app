e5a44969c6446b881d046a8d44b44bf9b1373fc5
Islombek <brilliantpul@gmail.com>
2023-06-05 22:10:43 +0500

    In work

---
+++ src/components/AppNav.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import AppLogo from '~/assets/AppLogo.svg'
+import AppCart from '~/assets/AppCart.svg'
+import AppUser from '~/assets/AppUser.png'
+</script>
+
+<template>
+  <div class="block w-full">
+    <nav class="flex items-center justify-between pb-4">
+      <div class="flex items-center space-x-15">
+        <img :src="AppLogo" alt="logo">
+        <ul class="hidden items-center sm:flex space-x-7">
+          <li>
+            <a href="#">Collections</a>
+          </li>
+          <li>
+            <a href="#">Men</a>
+          </li>
+          <li>
+            <a href="#">Women</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="flex items-center space-x-12">
+        <button>
+          <img :src="AppCart" alt="">
+        </button>
+        <button class="h-11 w-11 border-2 border-transparent rounded-full hover:border-orange-600">
+          <img :src="AppUser" alt="">
+        </button>
+      </div>
+    </nav>
+
+    <hr class="mt-3">
+  </div>
+</template>
---
+++ src/layouts/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <main
+    px-4 py-10
+    text="center gray-700 dark:gray-200"
+  >
+    <div class="mx-auto max-w-7xl w-full container">
+      <header class="w-full">
+        <AppNav />
+      </header>
+    </div>
+  </main>
+</template>
